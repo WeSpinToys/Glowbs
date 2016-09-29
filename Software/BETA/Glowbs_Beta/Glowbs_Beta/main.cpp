90eb5054c6afa869bfd521b0484bda4dae5d0587
superposition <ericmanganaro@gmail.com>
2016-09-29 18:59:31 -0400

    Atmel Debugging

---
+++ Software/BETA/Glowbs_Beta/Glowbs_Beta/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Glowbs_Beta.cpp
+ *
+ * Created: 9/29/2016 6:58:46 PM
+ * Author : User
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
